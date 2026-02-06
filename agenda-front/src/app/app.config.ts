e3ff71366680ca49270b300f50bfec77b08cf695
mgonpor <miguelgp2005@gmail.com>
2026-02-06 11:16:07 +0100

    git push

--- agenda-front/src/app/app.config.ts
+++ agenda-front/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
---
+++ agenda-front/src/app/features/grupos/services/grupo.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { GrupoRequest, GrupoResponse } from '../models/grupo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GrupoService {
+  private http = inject(HttpClient);
+  private readonly ADMIN_URL = `${environment.apiUrl}/admin/grupo`;
+  private readonly USER_URL = `${environment.apiUrl}/user/grupo`;
+
+  // --- MÉTODOS USER (Tu UserGrupoController) ---
+  getGruposUser(): Observable<GrupoResponse[]> {
+    return this.http.get<GrupoResponse[]>(this.USER_URL);
+  }
+
+  getGrupoUser(id: number): Observable<GrupoResponse> {
+    return this.http.get<GrupoResponse>(`${this.USER_URL}/${id}`);
+  }
+
+  createGrupoUser(grupo: GrupoRequest): Observable<any> {
+    return this.http.post(this.USER_URL, grupo);
+  }
+
+  updateGrupoUser(id: number, grupo: GrupoRequest): Observable<any> {
+    return this.http.put(`${this.USER_URL}/${id}`, grupo);
+  }
+
+  deleteGrupoUser(id: number): Observable<any> {
+    return this.http.delete(`${this.USER_URL}/${id}`);
+  }
+
+  // --- MÉTODOS ADMIN (Tu AdminGrupoController) ---
+  // Nota: Usamos HttpParams para el @RequestParam idUsuario de tu Java
+  getAllGruposAdmin(): Observable<GrupoResponse[]> {
+    return this.http.get<GrupoResponse[]>(this.ADMIN_URL);
+  }
+
+  createGrupoAdmin(grupo: GrupoRequest, idUsuario: number): Observable<any> {
+    const params = new HttpParams().set('idUsuario', idUsuario.toString());
+    return this.http.post(this.ADMIN_URL, grupo, { params });
+  }
+
+  deleteGrupoAdmin(id: number): Observable<string> {
+    // El backend devuelve un String plano, configuramos responseType
+    return this.http.delete(`${this.ADMIN_URL}/${id}`, { responseType: 'text' });
+  }
+}
